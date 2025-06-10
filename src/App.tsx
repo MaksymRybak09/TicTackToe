7cf9361310d352ee5691d69f68df97e9fa5c76c2
Maksym Rybak <maksymrybak09@gmail.com>
2025-06-10 20:06:30 +0300

    create: tick tack toue hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,101 +1,39 @@
-import { useEffect, useState } from "react";
 import "./App.css";
 import GameBoard from "./components/GameBoard/GameBoard";
 import GameControlls from "./components/GameControlls/GameControlls";
 import Modal from "./components/Modal/Modal";
 import PlayerInfo from "./components/PlayerInfo/PlayerInfo";
-import { checkWinner } from "./lib/helpers/chackWinner";
-import type { GameState } from "./types/gameState";
+import { useTickTackToue } from "./hooks/TickTackToue/useTickTackToue";
 
 function App() {
-  const [game, setGame] = useState<GameState>({
-    board: Array(3)
-      .fill(null)
-      .map(() => Array(3).fill(null)),
-    currentPlayer: "X",
-    winner: null,
-    gameCount: 0,
-    wins: { X: 0, O: 0 },
-    moveCount: 0,
-    gridSize: 3,
-    selectedGridSize: 3,
-  });
-  const [isModalShown, setIsModalShown] = useState(false);
-
-  useEffect(() => {
-    if (game.winner === "draw") {
-      setTimeout(() => setIsModalShown(true), 1000);
-    }
-  }, [game.winner]);
-
-  const handleCellClick = (row: number, col: number) => {
-    if (game.winner || game.board[row][col]) return;
-    const newBoard = game.board.map((row) => [...row]);
-    newBoard[row][col] = game.currentPlayer;
-
-    const newMoveCount = game.moveCount + 1;
-    const result = checkWinner(newBoard, game.currentPlayer, game.gridSize);
-
-    if (result) {
-      setTimeout(() => setIsModalShown(true), 1000);
-    }
-
-    setGame((prev) => ({
-      ...prev,
-      board: newBoard,
-      currentPlayer: prev.currentPlayer === "X" ? "O" : "X",
-      winner: result
-        ? result
-        : newMoveCount === game.gridSize ** 2
-        ? "draw"
-        : null,
-      moveCount: newMoveCount,
-      wins: result
-        ? {
-            ...prev.wins,
-            [prev.currentPlayer]: prev.wins[prev.currentPlayer] + 1,
-          }
-        : prev.wins,
-      gameCount:
-        result || newMoveCount === game.gridSize ** 2
-          ? prev.gameCount + 1
-          : prev.gameCount,
-    }));
-  };
-
-  const startNewGame = () => {
-    setGame((prev) => ({
-      ...prev,
-      board: Array(prev.selectedGridSize)
-        .fill(null)
-        .map(() => Array(prev.selectedGridSize).fill(null)),
-      currentPlayer: "X",
-      winner: null,
-      moveCount: 0,
-      gridSize: prev.selectedGridSize,
-    }));
-  };
+  const {
+    wins,
+    winner,
+    gameCount,
+    currentPlayer,
+    board,
+    selectedGridSize,
+    isModalShown,
+    handleCellClick,
+    startNewGame,
+    onGridSizeChange,
+    onModalClose,
+  } = useTickTackToue();
 
   return (
     <>
       <PlayerInfo
-        wins={game.wins}
-        gameCount={game.gameCount}
-        currentPlayer={game.currentPlayer}
+        wins={wins}
+        gameCount={gameCount}
+        currentPlayer={currentPlayer}
       />
-      <GameBoard board={game.board} onCeLLClick={handleCellClick} />
+      <GameBoard board={board} onCeLLClick={handleCellClick} />
       <GameControlls
         onNewGame={startNewGame}
-        selectedGridSize={game.selectedGridSize}
-        onGridSizeChange={(size: number) =>
-          setGame((prev) => ({ ...prev, selectedGridSize: size }))
-        }
+        selectedGridSize={selectedGridSize}
+        onGridSizeChange={onGridSizeChange}
       />
-      <Modal
-        show={isModalShown}
-        winner={game.winner}
-        onClose={() => setIsModalShown(false)}
-      />
+      <Modal show={isModalShown} winner={winner} onClose={onModalClose} />
     </>
   );
 }
---
+++ src/hooks/TickTackToue/useTickTackToue.ts
@@ -0,0 +1,87 @@
+import { useState, useEffect } from "react";
+import { checkWinner } from "../../lib/helpers/chackWinner";
+import type { GameState } from "../../types/gameState";
+
+export const useTickTackToue = () => {
+  const [game, setGame] = useState<GameState>({
+    board: Array(3)
+      .fill(null)
+      .map(() => Array(3).fill(null)),
+    currentPlayer: "X",
+    winner: null,
+    gameCount: 0,
+    wins: { X: 0, O: 0 },
+    moveCount: 0,
+    gridSize: 3,
+    selectedGridSize: 3,
+  });
+  const [isModalShown, setIsModalShown] = useState(false);
+
+  useEffect(() => {
+    if (game.winner === "draw") {
+      setTimeout(() => setIsModalShown(true), 1000);
+    }
+  }, [game.winner]);
+
+  const handleCellClick = (row: number, col: number) => {
+    if (game.winner || game.board[row][col]) return;
+    const newBoard = game.board.map((row) => [...row]);
+    newBoard[row][col] = game.currentPlayer;
+
+    const newMoveCount = game.moveCount + 1;
+    const result = checkWinner(newBoard, game.currentPlayer, game.gridSize);
+
+    if (result) {
+      setTimeout(() => setIsModalShown(true), 1000);
+    }
+
+    setGame((prev) => ({
+      ...prev,
+      board: newBoard,
+      currentPlayer: prev.currentPlayer === "X" ? "O" : "X",
+      winner: result
+        ? result
+        : newMoveCount === game.gridSize ** 2
+        ? "draw"
+        : null,
+      moveCount: newMoveCount,
+      wins: result
+        ? {
+            ...prev.wins,
+            [prev.currentPlayer]: prev.wins[prev.currentPlayer] + 1,
+          }
+        : prev.wins,
+      gameCount:
+        result || newMoveCount === game.gridSize ** 2
+          ? prev.gameCount + 1
+          : prev.gameCount,
+    }));
+  };
+
+  const startNewGame = () => {
+    setGame((prev) => ({
+      ...prev,
+      board: Array(prev.selectedGridSize)
+        .fill(null)
+        .map(() => Array(prev.selectedGridSize).fill(null)),
+      currentPlayer: "X",
+      winner: null,
+      moveCount: 0,
+      gridSize: prev.selectedGridSize,
+    }));
+  };
+
+  const onGridSizeChange = (size: number) =>
+    setGame((prev) => ({ ...prev, selectedGridSize: size }));
+
+  const onModalClose = () => setIsModalShown(false);
+
+  return {
+    ...game,
+    isModalShown,
+    handleCellClick,
+    startNewGame,
+    onGridSizeChange,
+    onModalClose,
+  };
+};
